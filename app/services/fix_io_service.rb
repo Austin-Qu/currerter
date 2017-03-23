f37c090b21d0c4e3489b8c0fce547d427744d56d
Anxing Qu <anxing.qu@gmail.com>
2017-03-23 11:39:03 +1100

    fix api

--- app/services/fix_io_service.rb
+++ app/services/fix_io_service.rb
@@ -5,7 +5,7 @@ class FixIoService
 
   base_uri 'api.fixer.io'
 
-  attr_reader :base, :symbols
+  attr_reader :base, :symbols, :amount
 
   def initialize(converter)
     raise NoneSupportedTypeError unless converter.is_a?(Converter)
@@ -15,13 +15,19 @@ class FixIoService
   end
 
   def execute
+    result ||= request_result
+    converted_amount = result['rates'][symbols] * amount.to_f
+    request_result.merge(converted_amount: converted_amount)
+  end
+
+  private
+
+  def request_result
     response = self.class.get('/latest', request_params)
     response.parsed_response
   rescue HTTParty::Error => e
     logger.error "HTTParty Error: #{e.message}"
   end
-
-  private
 
   def request_params
     { query: { base: base, symbols: symbols } }
